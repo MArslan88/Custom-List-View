9f86b00207621d5fcd2e299227aeb87c212a3010
MArslan88 <gery_maz_555@hotmail.com>
2022-12-02 11:47:07 +0500

    adapter implemented

---
+++ app/src/main/java/com/mhdarslan/listview/NameAdapter.java
@@ -0,0 +1,54 @@
+package com.mhdarslan.listview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class NameAdapter extends ArrayAdapter<NameModel> {
+    // invoke the suitable constructor of the ArrayAdapter class
+    public NameAdapter(Context context, ArrayList<NameModel> arrayList) {
+        // pass the context and arrayList for the super
+        // constructor of the ArrayAdapter class
+        super(context, 0, arrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // convertView which is recyclable view
+        View currentItemView = convertView;
+
+        // if the recyclable view is null then inflate the custom layout for the same
+        if (currentItemView == null) {
+            currentItemView = LayoutInflater.from(getContext()).inflate(R.layout.my_item, parent, false);
+        }
+
+        // get the position of the view from the ArrayAdapter
+        NameModel currentNumberPosition = getItem(position);
+
+        // then according to the position of the view assign the desired image for the same
+        ImageView numbersImage = currentItemView.findViewById(R.id.imageView);
+        assert currentNumberPosition != null;
+        numbersImage.setImageResource(currentNumberPosition.getImageId());
+
+        // then according to the position of the view assign the desired TextView 1 for the same
+        TextView textView1 = currentItemView.findViewById(R.id.fNameTV);
+        textView1.setText(currentNumberPosition.getFirstNameText());
+
+        // then according to the position of the view assign the desired TextView 2 for the same
+        TextView textView2 = currentItemView.findViewById(R.id.lNameTV);
+        textView2.setText(currentNumberPosition.getSecondNameText());
+
+        // then return the recyclable view
+        return currentItemView;
+    }
+}
