0ebfb0d9b854f6e4a8069875389d8ec9be4f1082
MArslan88 <gery_maz_555@hotmail.com>
2022-12-02 12:08:02 +0500

    item click listener step-6

--- app/src/main/java/com/mhdarslan/listview/MainActivity.java
+++ app/src/main/java/com/mhdarslan/listview/MainActivity.java
@@ -3,7 +3,10 @@ package com.mhdarslan.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         // add all the values from 1 to 15 to the arrayList
         // the items are of the type NameModel
         arrayList.add(new NameModel(R.mipmap.ic_launcher_round, "Muhammad", "Arslan"));
+        arrayList.add(new NameModel(R.mipmap.ic_launcher_round, "Danish", "Nisar"));
+        arrayList.add(new NameModel(R.mipmap.ic_launcher_round, "Tanzeel", "Khalique"));
+        arrayList.add(new NameModel(R.mipmap.ic_launcher_round, "Noman", "Mukhtar"));
 
 
         // Now create the instance of the NameAdapter and pass
@@ -31,5 +37,15 @@ public class MainActivity extends AppCompatActivity {
 
         // set the nameArrayAdapter for ListView
         mainListView.setAdapter(nameArrayAdapter);
+
+        // item click listener
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String fName = arrayList.get(position).getFirstNameText().toString();
+                String sName = arrayList.get(position).getSecondNameText().toString();
+                Toast.makeText(MainActivity.this,fName+ " " +sName , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
